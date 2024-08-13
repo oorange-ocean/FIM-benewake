7064f24c7427669e4c9095b37592732165333fd4
Gary <garymur301@gmail.com>
2024-08-13 14:50:23 +0800

    feat: 询单N次

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
 import {
     flexRender,
     useReactTable,
@@ -9,64 +9,99 @@ import {
     getPaginationRowModel,
     getSortedRowModel,
     getGroupedRowModel
-} from '@tanstack/react-table';
-import Paginate from './Paginate';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-import { useTableStatesContext, useUpdateTableStatesContext } from '../../hooks/useCustomContext';
-import DraggableHeader from './DraggableHeader';
-import { Modal, Box, Typography, Select, MenuItem } from '@mui/material';
+} from '@tanstack/react-table'
+import Paginate from './Paginate'
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import {
+    useTableStatesContext,
+    useUpdateTableStatesContext
+} from '../../hooks/useCustomContext'
+import DraggableHeader from './DraggableHeader'
+import { Modal, Box, Typography, Select, MenuItem } from '@mui/material'
 import { updateCustomerTypeReviseById } from '../../api/analysis'
 
-export default function Table({ data, columns, noPagination, setNewInquiryData, handleRefresh }) {
-    const states = useTableStatesContext();
-    const [rowSelection, setRowSelection] = useState({});
-    const columnVisibility = states.columnVisibility;
-    const [sorting, setSorting] = useState([]);
-    const [grouping, setGrouping] = useState([]);
-    const [columnFilters, setColumnFilters] = useState([]);
-    const [columnResizeMode] = useState('onChange');
-    const updateTableStates = useUpdateTableStatesContext();
-    const [columnOrder, setColumnOrder] = useState(columns.map(column => column.id));
+export default function Table({
+    data,
+    columns,
+    noPagination,
+    setNewInquiryData,
+    handleRefresh,
+    selected
+}) {
+    const states = useTableStatesContext()
+    const [rowSelection, setRowSelection] = useState({})
+    const columnVisibility = states.columnVisibility
+    const [sorting, setSorting] = useState([])
+    const [grouping, setGrouping] = useState([])
+    const [columnFilters, setColumnFilters] = useState([])
+    const [columnResizeMode] = useState('onChange')
+    const updateTableStates = useUpdateTableStatesContext()
+    const [columnOrder, setColumnOrder] = useState(
+        columns.map((column) => column.id)
+    )
 
-    const [modalOpen, setModalOpen] = useState(false);
-    const [currentCellData, setCurrentCellData] = useState('');
-    const [currentRowData, setCurrentRowData] = useState(null);
-    const labels = ['年度', '月度', '代理商', '新增', '临时', '日常'];
+    const [modalOpen, setModalOpen] = useState(false)
+    const [currentCellData, setCurrentCellData] = useState('')
+    const [currentRowData, setCurrentRowData] = useState(null)
+    const labels = ['年度', '月度', '代理商', '新增', '临时', '日常']
 
-    useEffect(() => setRowSelection({}), [data]);
-    useEffect(() => updateTableStates({ type: "SET_ROW_SELECTION", rowSelection }), [rowSelection]);
+    useEffect(() => setRowSelection({}), [data])
+    useEffect(
+        () => updateTableStates({ type: 'SET_ROW_SELECTION', rowSelection }),
+        [rowSelection]
+    )
 
-    const handleClose = () => setModalOpen(false);
+    const handleClose = () => setModalOpen(false)
     const handleChange = async (event) => {
-        const value = event.target.value;
-        // 调用/past-analysis/updateCustomerTypeReviseById?id=23&customerTypeRevise=请问 
-        await updateCustomerTypeReviseById(currentRowData.id, labels[value] ?? '');
+        const value = event.target.value
+        // 调用/past-analysis/updateCustomerTypeReviseById?id=23&customerTypeRevise=请问
+        await updateCustomerTypeReviseById(
+            currentRowData.id,
+            labels[value] ?? ''
+        )
         await handleRefresh()
-        setModalOpen(false);
-    };
+        setModalOpen(false)
+    }
 
     const getCellContent = (cell) => {
         // {cell.column.columnDef.id === 'monthAvg' ?
         //     cell.getContext().getValue() ? parseFloat(cell.getContext().getValue()).toFixed(2) : ''
         //     : flexRender(cell.column.columnDef.cell, cell.getContext())}
         if (cell.column.columnDef.id === 'monthAvg') {
-            return cell.getContext().getValue() ? parseFloat(cell.getContext().getValue()).toFixed(2) : '';
+            return cell.getContext().getValue()
+                ? parseFloat(cell.getContext().getValue()).toFixed(2)
+                : ''
+        } else if (cell.column.columnDef.id === 'state' && selected === 3) {
+            // 原始数据为询单N次
+            // 正则表达式提取n
+            const matchedValue = cell
+                .getContext()
+                .getValue()
+                .match(/-?\d+/)?.[0]
+            const n = parseInt(matchedValue || '-1', 10)
+            console.log(n)
+            switch (n) {
+                case -1:
+                    return '已删除'
+                case 0:
+                    return '保存'
+                default:
+                    return `询单${n}次`
+            }
         }
-        return flexRender(cell.column.columnDef.cell, cell.getContext());
-    };
+        return flexRender(cell.column.columnDef.cell, cell.getContext())
+    }
 
     const getCellStyleClass = (cell) => {
         //如果所在行的isYellow列是1，则返回cell-yellow
         if (cell.row.original?.isYellow === 1) {
-            return 'cell-yellow';
+            return 'cell-yellow'
+        } else if (cell.row.original?.oneIsYellow === 1) {
+            return 'cell-yellow'
         }
-        else if (cell.row.original?.oneIsYellow === 1) {
-            return 'cell-yellow';
-        }
-        return '';
+        return ''
     }
-
 
     const table = useReactTable({
         data,
@@ -87,8 +122,8 @@ export default function Table({ data, columns, noPagination, setNewInquiryData, 
         initialState: {
             columnVisibility: columnVisibility,
             pagination: {
-                pageSize: 100,
-            },
+                pageSize: 100
+            }
         },
         getCoreRowModel: getCoreRowModel(),
         getPaginationRowModel: getPaginationRowModel(),
@@ -104,27 +139,30 @@ export default function Table({ data, columns, noPagination, setNewInquiryData, 
         onColumnFiltersChange: setColumnFilters,
         meta: {
             updateData: (rowIndex, columnId, value) => {
-                setNewInquiryData(prev =>
+                setNewInquiryData((prev) =>
                     prev.map((row, index) => {
                         if (index === rowIndex) {
-                            return { ...prev[rowIndex], [columnId]: value };
+                            return { ...prev[rowIndex], [columnId]: value }
                         }
-                        return row;
+                        return row
                     })
-                );
-            },
-        },
-    });
+                )
+            }
+        }
+    })
 
     return (
         <DndProvider backend={HTML5Backend}>
             <div className="table-container col">
                 <div className="table-wrapper">
-                    <div className="table" style={{ width: table.getCenterTotalSize() }}>
-                        <div className='thead'>
-                            {table.getHeaderGroups().map(headerGroup => (
-                                <div className='tr' key={headerGroup.id}>
-                                    <div className='th checkbox fixed'>
+                    <div
+                        className="table"
+                        style={{ width: table.getCenterTotalSize() }}
+                    >
+                        <div className="thead">
+                            {table.getHeaderGroups().map((headerGroup) => (
+                                <div className="tr" key={headerGroup.id}>
+                                    <div className="th checkbox fixed">
                                         <input
                                             type="checkbox"
                                             name={table.id}
@@ -132,7 +170,7 @@ export default function Table({ data, columns, noPagination, setNewInquiryData, 
                                             onChange={table.getToggleAllRowsSelectedHandler()}
                                         />
                                     </div>
-                                    {headerGroup.headers.map(header => (
+                                    {headerGroup.headers.map((header) => (
                                         <DraggableHeader
                                             key={header.id}
                                             header={header}
@@ -142,13 +180,15 @@ export default function Table({ data, columns, noPagination, setNewInquiryData, 
                                 </div>
                             ))}
                         </div>
-                        <div className='tbody'>
-                            {table.getRowModel().rows.map(row => (
+                        <div className="tbody">
+                            {table.getRowModel().rows.map((row) => (
                                 <div
                                     key={row.id}
-                                    className={`tr${row.getIsSelected() ? ' selected' : ''}`}
+                                    className={`tr${
+                                        row.getIsSelected() ? ' selected' : ''
+                                    }`}
                                 >
-                                    <div className='td checkbox fixed'>
+                                    <div className="td checkbox fixed">
                                         <input
                                             type="checkbox"
                                             name={row.id}
@@ -156,16 +196,29 @@ export default function Table({ data, columns, noPagination, setNewInquiryData, 
                                             onChange={row.getToggleSelectedHandler()}
                                         />
                                     </div>
-                                    {row.getVisibleCells().map(cell => (
+                                    {row.getVisibleCells().map((cell) => (
                                         <div
                                             key={cell.id}
-                                            style={{ width: cell.column.getSize() }}
-                                            className={`td ${cell.column.columnDef.id} ${getCellStyleClass(cell)}`}
+                                            style={{
+                                                width: cell.column.getSize()
+                                            }}
+                                            className={`td ${
+                                                cell.column.columnDef.id
+                                            } ${getCellStyleClass(cell)}`}
                                             onDoubleClick={() => {
-                                                if (cell.column.columnDef.id === "customerTypeRevise") {
-                                                    setCurrentCellData(cell.getContext().getValue() ?? '');
-                                                    setCurrentRowData(row.original);
-                                                    setModalOpen(true);
+                                                if (
+                                                    cell.column.columnDef.id ===
+                                                    'customerTypeRevise'
+                                                ) {
+                                                    setCurrentCellData(
+                                                        cell
+                                                            .getContext()
+                                                            .getValue() ?? ''
+                                                    )
+                                                    setCurrentRowData(
+                                                        row.original
+                                                    )
+                                                    setModalOpen(true)
                                                 }
                                             }}
                                         >
@@ -180,21 +233,28 @@ export default function Table({ data, columns, noPagination, setNewInquiryData, 
                 {!noPagination && <Paginate table={table} />}
             </div>
             <Modal open={modalOpen} onClose={handleClose}>
-                <Box sx={{
-                    position: 'absolute',
-                    top: '50%',
-                    left: '50%',
-                    transform: 'translate(-50%, -50%)',
-                    width: 300,
-                    bgcolor: 'background.paper',
-                    boxShadow: 24,
-                    p: 4,
-                }}>
+                <Box
+                    sx={{
+                        position: 'absolute',
+                        top: '50%',
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)',
+                        width: 300,
+                        bgcolor: 'background.paper',
+                        boxShadow: 24,
+                        p: 4
+                    }}
+                >
                     <Typography variant="h6" component="h2">
                         客户类型转换
                     </Typography>
-                    <Select size='small'
-                        value={labels?.find(label => label == currentCellData) ? labels.indexOf(currentRowData?.customerType) : ''}
+                    <Select
+                        size="small"
+                        value={
+                            labels?.find((label) => label == currentCellData)
+                                ? labels.indexOf(currentRowData?.customerType)
+                                : ''
+                        }
                         onChange={handleChange}
                         fullWidth
                     >
@@ -203,12 +263,9 @@ export default function Table({ data, columns, noPagination, setNewInquiryData, 
                                 {label}
                             </MenuItem>
                         ))}
-
                     </Select>
-
-
                 </Box>
             </Modal>
         </DndProvider>
-    );
+    )
 }
--- src/routes/Inquiry.jsx
+++ src/routes/Inquiry.jsx
@@ -1,37 +1,43 @@
-import { useMemo, useState } from 'react';
+import { useMemo, useState } from 'react'
 import SecTabs from '../components/SecTabs'
-import Table from '../components/table/Table';
-import Views from '../components/Views';
-import Toolbar from '../components/Toolbar';
-import { useTableDataContext } from '../hooks/useCustomContext';
+import Table from '../components/table/Table'
+import Views from '../components/Views'
+import Toolbar from '../components/Toolbar'
+import { useTableDataContext } from '../hooks/useCustomContext'
 import { inquiryViews } from '../constants/Views'
-import AllDefs from '../constants/defs/AllDefs';
+import AllDefs from '../constants/defs/AllDefs'
+import { getTableId } from '../js/getData'
+import { useSelectedDataContext } from '../hooks/useCustomContext'
 
 // 全部订单
 export default function Inquiry() {
-  const tableData = useTableDataContext()
-  const columns = useMemo(() => AllDefs, [])
-  const features = ["delete", "export", "refresh", 'visibility']
-  const [views, setViews] = useState(inquiryViews)
+    const tableData = useTableDataContext()
+    const columns = useMemo(() => AllDefs, [])
+    const features = ['delete', 'export', 'refresh', 'visibility']
+    const [views, setViews] = useState(inquiryViews)
+    const [selected, setSelected] = useState()
 
-  return (
-    <div className='col full-screen'>
-      <div className="tab-contents">
-        <Toolbar features={features} />
-        <SecTabs />
-        <Views
-          views={views}
-          setViews={setViews}
-        />
-      </div>
-      {tableData &&
-        <div className='content-container col'>
-          <Table
-            data={tableData}
-            columns={columns}
-          />
+    return (
+        <div className="col full-screen">
+            <div className="tab-contents">
+                <Toolbar features={features} />
+                <SecTabs />
+                <Views
+                    views={views}
+                    setViews={setViews}
+                    selected={selected}
+                    setSelected={setSelected}
+                />
+            </div>
+            {tableData && (
+                <div className="content-container col">
+                    <Table
+                        data={tableData}
+                        columns={columns}
+                        selected={selected}
+                    />
+                </div>
+            )}
         </div>
-      }
-    </div>
-  )
+    )
 }
